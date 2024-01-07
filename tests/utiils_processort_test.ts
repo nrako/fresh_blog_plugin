511d686dfa7ec72d60a346e00bd58153cbcab428
nrako <nico@nrako.com>
2024-01-08 00:17:14 +0100

    test: add tests for markdown rendering result

---
+++ tests/utiils_processort_test.ts
@@ -0,0 +1,9 @@
+import processor from '../utils/processor.ts'
+import { assertSnapshot } from '$fresh/src/server/deps.ts'
+
+const text = await Deno.readTextFile('./tests/fixture/markdown_features.md')
+
+Deno.test('prodcue an expected HTML result', async (t) => {
+  const html =  await processor(text)
+  await assertSnapshot(t, html)
+})
