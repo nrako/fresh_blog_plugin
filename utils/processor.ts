d1fdb273d7833da83852538a70c4c0d00aad2ff7
nrako <nico@nrako.com>
2024-02-01 00:32:04 +0100

    feat(processor): support math with katex
    Needed to upgrade myst-parse and myst-to-html to v1.0.22 to fix the
    `.math-inline` and `.math-block` css classes.

--- utils/processor.ts
+++ utils/processor.ts
@@ -1,12 +1,14 @@
 import { transformerNotationDiff } from 'npm:shikiji-transformers@0.9'
 import { VFile } from 'https://esm.sh/vfile@5'
-import { mystParse } from 'https://esm.sh/myst-parser@1.0.21'
+import { mystParse } from 'https://esm.sh/myst-parser@1.0.22'
 import {
+  basicTransformationsPlugin,
   DOITransformer,
   GithubTransformer,
+  inlineMathSimplificationPlugin,
+  linksPlugin,
   RRIDTransformer,
   WikiTransformer,
-  linksPlugin,
 } from 'https://esm.sh/myst-transforms@1.1.19'
 import { unified } from 'https://esm.sh/unified@10'
 import { visit } from 'https://esm.sh/unist-util-visit@5'
@@ -15,7 +17,7 @@ import rehypeShiki from 'https://esm.sh/@shikijs/rehype@1.0.0-beta.3'
 import rehypeStringify from 'https://esm.sh/v135/rehype-stringify@8'
 import rehypeParse from 'https://esm.sh/rehype-parse@8'
 import rehypeExternalLinks from 'https://esm.sh/rehype-external-links@3'
-
+import rehypeKatex from 'https://esm.sh/v135/rehype-katex@7.0.0/lib/index.js'
 
 async function parse(text: string) {
   const file = new VFile()
@@ -35,6 +37,8 @@ async function parse(text: string) {
   // Also run some of the transforms, like the links
   const mdastPre = JSON.parse(JSON.stringify(mdast))
   unified()
+    .use(basicTransformationsPlugin, {})
+    .use(inlineMathSimplificationPlugin)
     .use(linksPlugin, { transformers: linkTransforms })
     .runSync(mdastPre)
 
@@ -60,9 +64,9 @@ async function parse(text: string) {
       ],
     })
     // @ts-ignore required until unified can be upgrade to v11 when mystmd plays fair
-    .use(rehypeExternalLinks, {rel: ['noopener']})
+    .use(rehypeExternalLinks, { rel: ['noopener'] })
     // @ts-ignore required until unified can be upgrade to v11 when mystmd plays fair
-    // .use(rehypeKatex)
+    .use(rehypeKatex)
     // @ts-ignore required until unified can be upgrade to v11 when mystmd plays fair
     .use(rehypeStringify)
     .process(htmlString)
