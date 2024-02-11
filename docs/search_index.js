1b7ace282cefcfacfae3854cc9be4a50c045e997
nrako <nico@nrako.com>
2024-02-11 23:17:15 +0100

    docs: untrack docs/ folder, the docs are deployed to GitHub pages
    The API documentation is generated from the source code and automatically
    deployed to GitHub pages when pushes on the `main` branch occurs by the
    `docs` Github workflow.

--- docs/search_index.js
+++
@@ -1,3 +0,0 @@
-(function () {
-  window.DENO_DOC_SEARCH_INDEX = {"nodes":[{"kind":["interface"],"name":"BlogOptions","file":".","location":{"filename":"main","line":7,"col":0,"byteIndex":286},"declarationKind":"export","deprecated":false},{"kind":["function"],"name":"default","file":".","location":{"filename":"main","line":109,"col":0,"byteIndex":3273},"declarationKind":"export","deprecated":false}]};
-})()
